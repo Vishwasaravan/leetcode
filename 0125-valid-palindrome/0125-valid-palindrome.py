a9dd6fd61795de1cc82129ef487cd53804bcebd7
Vishwa <vishwasaravanan586@gmail.com>
2026-07-22 09:11:27 +0530

    Time: 7 ms (81.25%), Space: 24 MB (5.25%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        org=[]
+        for i in s:
+            if i. isalnum():
+                 org.append(i. lower())
+        return org==org[::-1]
+        
+           
