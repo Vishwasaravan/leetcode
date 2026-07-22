274ea80273442b42da61a39a8a3a9c740d6c620b
Vishwa <vishwasaravanan586@gmail.com>
2026-07-22 08:58:01 +0530

    Time: 53 ms (15.34%), Space: 19.1 MB (69.47%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverse(self,x:int) ->int:
+        z=x
+        r=0
+        if x<0:
+            x*=-1
+        while x!=0:
+            h=x%10
+            r=r*10+h
+            x//=10
+        if z<0:
+                r*=-1
+        if -2**31<r<2**31-1:
+             return r
+        else:
+            return 0
+         
+
+        
+        
+        
